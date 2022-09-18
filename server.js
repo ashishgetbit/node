d27fa62d1627799b59a86bc47d04c2df30bec2b6
Ashish Raj <ashishraj@getbit.money>
2022-09-18 20:14:20 +0530

    feat:add dotenv

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
-
+const dotenv = require('dotenv')
+dotenv.config()
 const app = express()
 
 app.get('/', (req, res) => {
    res.send("SErver is running")
 }
 )
-const PORT = 5001 || process.env.PORT
+const PORT = process.env.PORT || 5001
 app.listen(PORT, () => {
     console.log(`server is running in ${process.env.NODE_ENV} on port ${PORT}`)
 })
